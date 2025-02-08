68755b57cad651d8393273dd77f6092d63a8d4df
khoa.trandang2 <khoa.trandang2@di4l.vn>
2025-02-08 11:25:44 +0700

    add app bar

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import eslint from 'vite-plugin-eslint'
+import svgr from 'vite-plugin-svgr'
 
 export default defineConfig({
-  plugins: [react(), eslint()],
+  optimizeDeps: {
+    include: ['@emotion/react', '@emotion/styled', '@mui/material/Tooltip']
+  },
+  plugins: [
+    react(),
+    eslint(),
+    svgr()
+  ],
   resolve: {
     alias: [
       { find: '~', replacement: '/src' }
