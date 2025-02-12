e725f57d7a79d1432aba999b8f5e7dad01d49b24
khoa.trandang2 <khoa.trandang2@di4l.vn>
2025-02-12 10:39:52 +0700

    refactor code structure

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -37,7 +37,6 @@ function BoardBar() {
       paddingX: 2,
       overflowX: 'auto',
       bgcolor: (theme) => (theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'),
-      borderBottom: '1px solid white'
     }}>
       <Box sx={{ display:'flex', alignItems:'center', gap: 2 }}>
         <Chip
